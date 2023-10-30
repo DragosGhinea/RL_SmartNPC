36bf39a7eb0a34346ec29a92a3ee2edb9c4c58f3
DragosGhinea <dragos.dev12@gmail.com>
2023-10-31 01:11:13 +0200

    Added method to action for continuous support

--- src/main/java/ro/smartnpc/npc/actions/Action.java
+++ src/main/java/ro/smartnpc/npc/actions/Action.java
@@ -7,4 +7,8 @@ public interface Action {
     ActionType getActionType();
 
     void execute(EnvironmentNPC npc);
+
+    default void executeContinuous(EnvironmentNPC npc, double... args) {
+        execute(npc);
+    }
 }
